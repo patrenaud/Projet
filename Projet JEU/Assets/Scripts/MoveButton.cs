ee5d6fbd937df16c11b589e3778e37f5f318a9ed
Patrick R <1810057@bart.ca>
2018-05-23 15:58:38 -0400

    Raycast Move and Attack

---
+++ Projet JEU/Assets/Scripts/MoveButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveButton : MonoBehaviour
+{
+    
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayon = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hitinfo;
+
+            if (Physics.Raycast(rayon, out Hitinfo, 100f, LayerMask.GetMask("Enemy")))
+            {
+                Debug.Log("Attack");
+            }
+            else if (Physics.Raycast(rayon, out Hitinfo, 100f, LayerMask.GetMask("Ground")))
+            {
+                Debug.Log("Move");
+            }
+        }
+    }
+}
---
+++ Projet JEU/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public List<GameObject> m_Characters = new List<GameObject>();
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+}
