2d641badd79e6878eb0257f7006e92c7dbd865de
Patrick R <1810057@bart.ca>
2018-05-24 12:42:10 -0400

    Models included for map. Need to change to prefabs

--- Projet JEU/Assets/Scripts/Script.cs
+++ Projet JEU/Assets/Scripts/Script.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Script : MonoBehaviour {
+public class Script : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+
+	void Update ()
+    {
 		
 	}
 }
