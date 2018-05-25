248f2316cf84c361d0602171c50ffe8efa193b06
Patrick R <1810057@bart.ca>
2018-05-25 15:47:21 -0400

    Prefabs DONE TurnManager Started UI Started

--- Projet JEU/Assets/Scripts/MoveButton.cs
+++ Projet JEU/Assets/Scripts/MoveButton.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class MoveButton : MonoBehaviour
 {
-    
+    // private bool m_ActivateMove = false;
+
+    private void Start()
+    {
+
+    }
 
     private void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray rayon = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit Hitinfo;
 
-            if (Physics.Raycast(rayon, out Hitinfo, 100f, LayerMask.GetMask("Enemy")))
+            if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Enemy")))
             {
                 Debug.Log("Attack");
             }
-            else if (Physics.Raycast(rayon, out Hitinfo, 100f, LayerMask.GetMask("Ground")))
+            else if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Ground")))
             {
                 Debug.Log("Move");
             }
--- Projet JEU/Assets/Scripts/TurnManager.cs
+++ Projet JEU/Assets/Scripts/TurnManager.cs
@@ -6,14 +6,29 @@ public class TurnManager : MonoBehaviour
 {
     public List<GameObject> m_Characters = new List<GameObject>();
 
-	void Start ()
-    {
-		
-	}
-	
+    private bool m_SwitchCharacter = false;
+    private int m_Turn = 1;
 
-	void Update ()
+    private void Update()
     {
-		
-	}
+        if (m_SwitchCharacter == true)
+        {
+            if (m_Turn < m_Characters.Count)
+            {
+                m_Characters[m_Turn].gameObject.SetActive(false);
+                m_Characters[m_Turn + 1].gameObject.SetActive(true);
+				Debug.Log(m_Characters[m_Turn]);
+                m_Turn++;                
+            }
+            else
+            {
+                m_Characters[m_Turn].gameObject.SetActive(false);
+                m_Turn = 1;
+                m_Characters[m_Turn].gameObject.SetActive(true);
+                Debug.Log(m_Characters[m_Turn]);
+            }
+
+            m_SwitchCharacter = false;
+        }
+    }
 }
