8456f41da0666dcf8837fe8e1232c6dbe539e764
Patrick R <1810057@bart.ca>
2018-05-28 17:00:43 -0400

    Buttons beginning

---
+++ Projet JEU/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool m_CanMove = false;
+    public bool m_CanAttack = false;
+    public bool m_CanHability = false;
+    public Button m_AttackButton;
+    public Button m_MoveButton;
+    public Button m_AbilityButton;
+
+
+    private void Update()
+    {
+        if (m_CanMove)
+        {
+            Move();
+        }
+        else if (m_CanAttack)
+        {
+            Attack();
+        }
+        else if (m_CanHability)
+        {
+            //Hability();
+        }
+
+
+    }
+
+    public void Move()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayon = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hitinfo;
+
+            if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Player")))
+            {
+                Debug.Log("Invalid Move");
+
+            }
+            else if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Enemy")))
+            {
+                Debug.Log("Invalid Move");
+
+            }
+            else if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Ground")))
+            {
+                Debug.Log("Move");
+                m_CanMove = false;
+                m_MoveButton.interactable = false;
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayon = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hitinfo;
+
+            if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Enemy")))
+            {
+                Debug.Log("Attack");
+                m_CanAttack = false;
+                m_AttackButton.interactable = false;
+            }
+        }
+    }
+
+    public void EndTurn()
+    {
+
+        Debug.Log("End Turn");
+    }
+
+
+    #region Activatables
+    public void ActivateMove()
+    {
+        m_CanMove = true;
+        Debug.Log("CanMove");
+    }
+
+    public void ActivateAttack()
+    {
+        m_CanAttack = true;
+        Debug.Log("CanAttack");
+    }
+
+    public void ActivateHabilty()
+    {
+        m_CanHability = true;
+        Debug.Log("Hability");
+    }
+    #endregion
+}
--- Projet JEU/Assets/Scripts/TurnManager.cs
+++ Projet JEU/Assets/Scripts/TurnManager.cs
@@ -1,34 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnManager : MonoBehaviour
 {
     public List<GameObject> m_Characters = new List<GameObject>();
-
-    private bool m_SwitchCharacter = false;
+    public Button m_AttackButton;
+    public Button m_MoveButton;
+    public Button m_AbilityButton;
+    public bool m_SwitchCharacter = false;
     private int m_Turn = 1;
 
     private void Update()
     {
-        if (m_SwitchCharacter == true)
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_SwitchCharacter = true;            
+        }
+
+        if (m_SwitchCharacter)
         {
             if (m_Turn < m_Characters.Count)
             {
-                m_Characters[m_Turn].gameObject.SetActive(false);
-                m_Characters[m_Turn + 1].gameObject.SetActive(true);
+                //m_Characters[m_Turn - 1].gameObject.SetActive(false);
+                //m_Characters[m_Turn].gameObject.SetActive(true);
 				Debug.Log(m_Characters[m_Turn]);
-                m_Turn++;                
+
+                //*******   Le Script doit envoyer un appel pour débuter le tour au joueur/ennemi actif */
+
+                m_Turn++;                            
             }
             else
             {
-                m_Characters[m_Turn].gameObject.SetActive(false);
+                //m_Characters[m_Turn].gameObject.SetActive(false);
                 m_Turn = 1;
-                m_Characters[m_Turn].gameObject.SetActive(true);
-                Debug.Log(m_Characters[m_Turn]);
+                //m_Characters[m_Turn - 1].gameObject.SetActive(true);
+                Debug.Log(m_Characters[m_Turn - 1]);
+                
+                //*******   Le Script doit envoyer un appel pour débuter le tour au joueur */
+                m_AttackButton.interactable = true;
+                m_MoveButton.interactable = true;
+                m_AbilityButton.interactable = true;
+
             }
 
             m_SwitchCharacter = false;
         }
+    }
+
+    public void ActivateSwitchCharacter()
+    {
+        m_SwitchCharacter = true;
     }
 }
