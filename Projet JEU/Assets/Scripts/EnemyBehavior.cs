f108a1f782fa6ca0e96ecc6a1aba5cdfcb0c9996
Patrick R <1810057@bart.ca>
2018-05-31 08:40:31 -0400

    move/attack zone

---
+++ Projet JEU/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public bool m_Attackable = false;
+
+    private void OnTriggerStay(Collider a_Other)
+    {
+        if (a_Other.gameObject.layer == LayerMask.NameToLayer("Interractible"))
+        {
+            a_Other.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+            m_Attackable = true;
+        }
+    }
+
+    private void OnTriggerLeave(Collider a_Other)
+    {
+        if (a_Other.gameObject.layer == LayerMask.NameToLayer("Interractible"))
+        {
+            a_Other.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            m_Attackable = false;
+        }
+    }
+}
--- Projet JEU/Assets/Scripts/PlayerController.cs
+++ Projet JEU/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,24 @@ public class PlayerController : MonoBehaviour
 {
     public bool m_CanMove = false;
     public bool m_CanAttack = false;
-    public bool m_CanHability = false;
+    public bool m_CanAbility = false;
     public Button m_AttackButton;
     public Button m_MoveButton;
     public Button m_AbilityButton;
+    public float m_MoveSpeed = 5f;
 
+    public GameObject m_MovePrefab;
+    public GameObject m_AttackPrefab;
+
+    public Vector3 m_Position;
+
+
+    private void Awake()
+    {
+        m_MovePrefab.SetActive(false);
+        m_AttackPrefab.SetActive(false);
+
+    }
 
     private void Update()
     {
@@ -23,11 +36,14 @@ public class PlayerController : MonoBehaviour
         {
             Attack();
         }
-        else if (m_CanHability)
+        else if (m_CanAbility)
         {
-            //Hability();
+            Ability();
         }
+    }
 
+    private void FixedUpdate()
+    {
 
     }
 
@@ -48,14 +64,41 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("Invalid Move");
 
             }
-            else if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Ground")))
+            else if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("UI")))
+            {
+                Debug.Log("Cancel Move");
+                m_CanMove = false;
+            }
+            else if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Ground")) && Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Interractible")))
             {
                 Debug.Log("Move");
+                m_Position.x = Hitinfo.point.x;
+                m_Position.z = Hitinfo.point.z;
+                m_Position.y = transform.position.y;
+
+                StartCoroutine(MovetoPoint());
                 m_CanMove = false;
                 m_MoveButton.interactable = false;
+                m_MovePrefab.SetActive(false);
             }
         }
     }
+
+    private IEnumerator MovetoPoint()
+    {
+        float Timer = 0f;
+        {
+            transform.LookAt(m_Position);
+            while (Vector3.Distance(m_Position, transform.position) > 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, m_Position, Timer);
+
+                Timer += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+
 
     public void Attack()
     {
@@ -66,11 +109,23 @@ public class PlayerController : MonoBehaviour
 
             if (Physics.Raycast(rayon, out Hitinfo, 500f, LayerMask.GetMask("Enemy")))
             {
-                Debug.Log("Attack");
-                m_CanAttack = false;
-                m_AttackButton.interactable = false;
+                // If bool de trigger de l'enemy est actif
+                //if (GetComponent<EnemyBehavior>().m_Attackable)
+                {
+                    Debug.Log("Attack");
+                    m_CanAttack = false;
+                    m_AttackButton.interactable = false;
+                    m_AttackPrefab.SetActive(false);
+                }
             }
         }
+    }
+
+    public void Ability()
+    {
+        Debug.Log(" Hability ");
+        m_CanAbility = false;
+        m_AbilityButton.interactable = false;
     }
 
     public void EndTurn()
@@ -85,17 +140,20 @@ public class PlayerController : MonoBehaviour
     {
         m_CanMove = true;
         Debug.Log("CanMove");
+        m_MovePrefab.SetActive(true);
+        //Instantiate(m_MovePrefab, transform.gameObject.transform.position, transform.gameObject.transform.rotation);
     }
 
     public void ActivateAttack()
     {
         m_CanAttack = true;
         Debug.Log("CanAttack");
+        m_AttackPrefab.SetActive(true);
     }
 
     public void ActivateHabilty()
     {
-        m_CanHability = true;
+        m_CanAbility = true;
         Debug.Log("Hability");
     }
     #endregion
